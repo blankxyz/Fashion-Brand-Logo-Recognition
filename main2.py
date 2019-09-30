22f3081fc52291a07c5e95d772cd69e71aced5e0
gosu1 <55071790+gosu1@users.noreply.github.com>
2019-09-30 16:38:52 +0900

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+from skimage import io
+
+import urllib.request
+
+urllib.request.urlretrieve(url, "jjj.png")
+
+
+url = "http://hiphople.com/files/attach/images/6572924/694/211/006/52547a15db86233d80f0ee1f88c000a5.jpg"
+
